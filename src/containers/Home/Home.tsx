2ba10112e77a2434bb4fd7dd6750a12d80a3f395
Mariusz Siwek <lordh@wp.pl>
2019-08-21 11:28:40 +0200

    Initial commit

---
+++ src/containers/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = (): React.ReactElement => {
+  return (
+    <div
+      style={{
+        textAlign: "center"
+      }}
+    >
+      <h1>Blokchain Analytic Dashboard</h1>
+      <h3><a href="https://github.com/ScalaConsultants/Blokchain-Analytic-Dashboard">https://github.com/ScalaConsultants/Blokchain-Analytic-Dashboard</a></h3>
+    </div>
+  );
+};
+
+export default Home;
