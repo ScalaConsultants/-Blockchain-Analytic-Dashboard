7fdc3117b9a74867acede30b537224536ac58830
Wojciech Nowakowski <wojciech.nowakowski94@gmail.com>
2019-08-28 16:54:16 +0200

    Init treemap container

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -5,6 +5,8 @@ import Charts from "../containers/Charts/Charts";
 import Home from "../containers/Home/Home";
 import LiveChart from "../containers/LiveChart/LiveChart";
 import LiveChart2 from "../containers/LiveChart2/LiveChart2";
+import Treemap1 from "../containers/Treemap/Treemap";
+
 import { Container } from "@material-ui/core";
 
 export default (): React.ReactElement => {
@@ -13,6 +15,8 @@ export default (): React.ReactElement => {
       <Route exact path="/" component={Home} />
       <Route exact path="/transactions" component={Transactions} />
       <Route exact path="/charts" component={Charts} />
+      <Route exact path="/treemap" component={Treemap1} />
+
       <Route exact path="/top-seller" component={LiveChart} />
       <Route exact path="/top-buyer" component={LiveChart2} />
     </Container>
