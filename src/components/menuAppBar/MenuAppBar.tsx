40c2f25cf1580fe021ed3ae6190c640ede5d5e48
alanw <alan.wolejko@scalac.io>
2019-08-29 20:19:53 +0200

    refactor: remove custom chart view

--- src/components/menuAppBar/MenuAppBar.tsx
+++ src/components/menuAppBar/MenuAppBar.tsx
@@ -18,7 +18,6 @@ const ButtonAppBar = (props: any): React.ReactElement => {
     { name: 'Charts', route: '/charts' },
     { name: 'Top seller', route: '/top-seller' },
     { name: 'Top buyer', route: '/top-buyer' },
-    { name: 'Custom chart', route: '/custom-chart' },
   ];
 
   const goTo = (route: string): void => {
