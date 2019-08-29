fe8ebac10966ecc024d8cf95439931731158fd53
alanw <alan.wolejko@scalac.io>
2019-08-29 11:04:16 +0200

    feat: add some styling

--- src/components/customCharts/CustomChart.tsx
+++ src/components/customCharts/CustomChart.tsx
@@ -1,11 +1,27 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import RemoveIcon from '@material-ui/icons/Remove';
+import AddIcon from '@material-ui/icons/Add';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
 
 interface Props {
   chartData: any[];
   fullData: any[];
 }
 
-const CHART_WIDTH = 1300;
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(1),
+    margin: theme.spacing(2, 0),
+  },
+}));
+
+const CHART_WIDTH = 1200;
 const CHART_HEIGHT = 400;
 const CHART_PADDING = 10;
 
@@ -39,7 +55,9 @@ const CustomChart = ({
   const canvasRef = useRef(null);
   const [scale, setScale] = useState(0.2);
   const [canvasObjects, setCanvasObjects] = useState([]);
-  const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedRecordKey, setSelectedRecordKey] = useState(null);
+
+  const classes = useStyles();
 
   useEffect(() => {
     if (canvasRef.current) {
@@ -56,17 +74,16 @@ const CustomChart = ({
           value: e.value,
           key: e.key,
         };
-        ctx.fillStyle = selectedItem === e.key ? 'rgb(44,123,200)' : 'rgb(98,156,200)';
-        console.log(element.x, element.y, element.width, element.height)
+        ctx.fillStyle = selectedRecordKey === e.key ? 'rgb(44,123,200)' : 'rgb(98,156,200)';
         ctx.fillRect(element.x, element.y, element.width, element.height);
         return element
       });
       // @ts-ignore
       setCanvasObjects(objects)
     }
-  }, [scale, chartData, selectedItem]);
+  }, [scale, chartData, selectedRecordKey]);
 
-  const selectedRecord = fullData.find((element: any) => element.destination === selectedItem) || {};
+  const selectedRecord = fullData.find((element: any) => element.destination === selectedRecordKey) || {};
 
   return (
     <div style={{
@@ -82,28 +99,77 @@ const CustomChart = ({
           border: '1px solid black',
           width: CHART_WIDTH,
           height: CHART_HEIGHT,
+          marginBottom: 10,
         }}
         ref={canvasRef}
-        onClick={(e) => handleElementClick(setSelectedItem, canvasObjects, getClickPosition(e, canvasRef.current))}
+        onClick={(e) => handleElementClick(setSelectedRecordKey, canvasObjects, getClickPosition(e, canvasRef.current))}
       >
         This element is not supported by your browser.
       </canvas>
-      <input
-        type="number"
-        value={scale * 10}
-        onChange={e => setScale(parseInt(e.target.value, 10) / 10)}
-      />
-      <div>
-        <p>Selected record details:</p>
-        <p>destination: {selectedRecord.destination}</p>
-        <p>source: {selectedRecord.source}</p>
-        <p>amount: {selectedRecord.amount}</p>
-        <p>date: {new Date(selectedRecord.timestamp).toDateString()}</p>
-        <p>block level: {selectedRecord.block_level}</p>
-        <p>counter: {selectedRecord.counter}</p>
-        <p>fee: {selectedRecord.fee}</p>
-        <p>transactions: {selectedRecord.transactions}</p>
-      </div>
+      <Paper
+        className={classes.root}
+      >
+        <span>Zoom {Math.floor(scale * 10)}</span>
+        <IconButton size="small" color="secondary" aria-label="minus">
+          <RemoveIcon onClick={() => scale >= 0.2 ? setScale(scale - 0.1) : null} />
+        </IconButton>
+        <IconButton size="small" color="secondary" aria-label="plus">
+          <AddIcon onClick={() => setScale(scale + 0.1)} />
+        </IconButton>
+      </Paper>
+      {selectedRecordKey && (
+        <Paper className={classes.root}>
+          <h3>Selected record details</h3>
+          <Grid container spacing={3}>
+            <Grid item xs={6}>
+              <List>
+                <ListItem>
+                  <h4 style={{ marginRight: 10 }}>destination</h4>
+                  {selectedRecord.destination}
+                </ListItem>
+                <Divider light />
+                <ListItem>
+                  <h4 style={{ marginRight: 10 }}>source</h4>
+                  {selectedRecord.source}
+                </ListItem>
+                <Divider light />
+                <ListItem>
+                  <h4 style={{ marginRight: 10 }}>transactions</h4>
+                  {selectedRecord.transactions}
+                </ListItem>
+                <Divider light />
+                <ListItem>
+                  <h4 style={{ marginRight: 10 }}>amount</h4>
+                  {selectedRecord.amount}
+                </ListItem>
+              </List>
+            </Grid>
+            <Grid item xs={6}>
+              <List>
+                <ListItem>
+                  <h4 style={{ marginRight: 10 }}>date</h4>
+                  {new Date(selectedRecord.timestamp).toDateString()}
+                </ListItem>
+                <Divider light />
+                <ListItem>
+                  <h4 style={{ marginRight: 10 }}>block level</h4>
+                  {selectedRecord.block_level}
+                </ListItem>
+                <Divider light />
+                <ListItem>
+                  <h4 style={{ marginRight: 10 }}>counter</h4>
+                  {selectedRecord.counter}
+                </ListItem>
+                <Divider light />
+                <ListItem>
+                  <h4 style={{ marginRight: 10 }}>fee</h4>
+                  {selectedRecord.fee}
+                </ListItem>
+              </List>
+            </Grid>
+          </Grid>
+        </Paper>
+      )}
     </div>
   );
 };
