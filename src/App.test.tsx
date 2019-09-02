96446e062deb9cb5ca815ab066f96dd4af84dafa
Agata Kulbicka <agata.kulbicka@scalac.io>
2019-09-02 14:12:24 +0200

    BV-8 App - fixed failing test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { StoreContext } from "redux-react-hook";
+import configureStore from "./store";
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const store = configureStore();
+
+  ReactDOM.render(
+    <StoreContext.Provider
+      value={store}>
+      <App />
+    </StoreContext.Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
