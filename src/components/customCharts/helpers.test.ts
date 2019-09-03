370d6f33171bf5c5fc7fe1aedeb3d56d436ef50e
Agata Kulbicka <agata.kulbicka@scalac.io>
2019-09-03 10:17:42 +0200

    BV-8 CustomColumnChart helpers - added unit tests

---
+++ src/components/customCharts/helpers.test.ts
@@ -0,0 +1,24 @@
+import { getClickPosition } from './helpers';
+
+
+describe('CustomColumnChart helpers', () => {
+
+  const e = {
+    pageX: 10,
+    pageY: 20
+  };
+
+  const canvas = {
+    offsetLeft: 5,
+    offsetTop: 4
+  };
+
+  const result = {
+    x: 5,
+    y: 16
+  };
+
+  it('should return proper value', () => {
+    expect(getClickPosition(e, canvas)).toEqual(result)
+  });
+});
