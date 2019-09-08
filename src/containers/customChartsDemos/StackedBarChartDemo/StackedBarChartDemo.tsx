5aad9084aa428b8fdb4589f8aeb9824b2e2eca83
alanw <alan.wolejko@scalac.io>
2019-09-08 16:41:29 +0200

    refactor: code style corrections

--- src/containers/customChartsDemos/StackedBarChartDemo/StackedBarChartDemo.tsx
+++ src/containers/customChartsDemos/StackedBarChartDemo/StackedBarChartDemo.tsx
@@ -10,27 +10,31 @@ const mapState = (state: any): any => ({
 function SimpleBarChartDemo(): React.ReactElement {
   const { blokchain } = useMappedState(mapState);
 
-  if (!blokchain.length) return <div />;
-  const totalWalletsAmounts = (groupingKey: string) => blokchain.reduce((acc: any, next: any): any => {
-    const alreadyExist = acc.find((item: any) => item.key === next[groupingKey]);
-    if (alreadyExist) {
-      alreadyExist.value += next.amount;
-      return acc;
-    }
-    const newItem = {
-      key: next[groupingKey],
-      value: next.amount,
-    };
+  const totalWalletsAmounts = (groupingKey: string) =>
+    blokchain.reduce((acc: any, next: any): any => {
+      const alreadyExist = acc.find(
+        (item: any) => item.key === next[groupingKey]
+      );
+      if (alreadyExist) {
+        alreadyExist.value += next.amount;
+        return acc;
+      }
+      const newItem = {
+        key: next[groupingKey],
+        value: next.amount,
+      };
 
-    return [...acc, newItem];
-  }, []);
+      return [...acc, newItem];
+    }, []);
 
   return (
     <>
       <h1 style={{ marginBottom: 60 }}>Stacked bar chart demo</h1>
 
       <h2>Top buyers and sellers</h2>
-      <h3>Wallets by total transactions values and its relation to the entire downloaded range</h3>
+      <h3>
+        Wallets by total transactions values and its relation to the entire downloaded range
+      </h3>
 
       <h4 style={{ marginBottom: 12 }}>Buying transactions</h4>
       <StackedBarChart
@@ -45,8 +49,15 @@ function SimpleBarChartDemo(): React.ReactElement {
       <h2>Total transactions values</h2>
       <SimpleHorizontalBarChart
         data={[
-          {name: 'Buying', value: totalWalletsAmounts('source').reduce((acc: any, next: any) => acc + next.value, 0)},
-          {name: 'Selling', value: totalWalletsAmounts('destination').reduce((acc: any, next: any) => acc + next.value, 0)},
+          {
+            name: 'Buying',
+            value: totalWalletsAmounts('source').reduce((acc: any, next: any) => acc + next.value, 0)
+          },
+          {
+            name: 'Selling',
+            value: totalWalletsAmounts('destination').reduce((acc: any, next: any) => acc + next.value, 0)
+          },
+
         ]}
       />
       <p>Value in USD divided by 1000000000</p>
