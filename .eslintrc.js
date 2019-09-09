d2045fd2bee0a9598977c28ae61d0e5c7366bdd9
Agata Kulbicka <agata.kulbicka@scalac.io>
2019-09-09 10:11:40 +0200

    BV-19 Lint -updated linterc rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   extends: [
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    "plugin:react/recommended", // Mark jsx as using something from imports
-    "prettier",
-    "prettier/react",
-    "airbnb",
-    "airbnb/hooks",
+    // "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    // "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    // "plugin:react/recommended", // Mark jsx as using something from imports
+    // "prettier",
+    // "prettier/react",
+    // "airbnb",
+    // "airbnb/hooks",
   ],
   "plugins": ["prettier"],
   parserOptions: {
@@ -15,24 +15,24 @@ module.exports = {
     sourceType: "module" // Allows for the use of imports
   },
   rules: {
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "arrow-parens": "off",
+    // "@typescript-eslint/explicit-function-return-type": "off",
+    // "arrow-parens": "off",
     "comma-dangle": ["error", "never"],
-    "import/no-extraneous-dependencies": ["error", { "devDependencies": true }],
+    // "import/no-extraneous-dependencies": ["error", { "devDependencies": true }],
     "import/no-unresolved": [ 0, { caseSensitive: false } ],
     "no-console": 2,
-    "no-plusplus": "off",
+    // "no-plusplus": "off",
     "object-curly-spacing": ["error", "never"],
-    "prettier/prettier": "error",
+    // "prettier/prettier": "error",
     "quotes": ["error", "single"],
-    "react/jsx-curly-spacing": ["error", {
-      "attributes": { "when": "never" },
-      "children": { "when": "never" },
-      "allowMultiline": true
-    }],
-    "react/destructuring-assignment":"off",
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".ts", ".tsx"] }],
-    "react/jsx-props-no-spreading": "off"
+    // "react/jsx-curly-spacing": ["error", {
+    //   "attributes": { "when": "never" },
+    //   "children": { "when": "never" },
+    //   "allowMultiline": true
+    // }],
+    // "react/destructuring-assignment":"off",
+    // "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".ts", ".tsx"] }],
+    // "react/jsx-props-no-spreading": "off"
   },
   env: {
     "browser": true,
