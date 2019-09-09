61e9606ac8cac55e7af22dae4c8b83cae3fdcc6a
Monika Grabowska <you@example.com>
2019-09-09 16:24:13 +0200

    changes in Menu Items

--- src/constant.tsx
+++ src/constant.tsx
@@ -8,13 +8,17 @@ import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
 import MonetizationOnOutlinedIcon from '@material-ui/icons/MonetizationOnOutlined';
 import DashboardOutlinedIcon from '@material-ui/icons/DashboardOutlined';
 import AddToPhotosOutlinedIcon from '@material-ui/icons/AddToPhotosOutlined';
+import TimelineIcon from '@material-ui/icons/Timeline';
 
 export const menuItems = [
-    { name: "Home", route: "/", icon: <Home />, description: "Home Page"},
-    { name: "Transactions", route: "/transactions", icon: <ListIcon /> , description: "List of the last 100 000 transaction"},
+    { name: "Home", route: "/", icon: <Home />, description: "Home Page" },
+    { name: "Transactions", route: "/transactions", icon: <ListIcon />, description: "List of the last 100 000 transaction" },
     { name: "Charts", route: "/charts", icon: <EqualizerIcon />, description: "Graphic representaion of the data" },
     { name: "Top seller", route: "/top-seller", icon: <MonetizationOnIcon />, description: "50 sellers who made the most transactions" },
     { name: "Top buyer", route: "/top-buyer", icon: <MonetizationOnOutlinedIcon />, description: "50 buyers who made the most transactions" },
     { name: "Treemap", route: "/treemap", icon: <DashboardOutlinedIcon />, description: "Concept graph" },
-    { name: "Custom chart", route: "/top-buyer-with-custom", icon: <AddToPhotosOutlinedIcon />, description: "Alan's proposal"}
+    { name: 'Line chart', route: '/line-chart' , icon: <TimelineIcon />, description: "Line Chart"},
+    { name: 'Simple Bar Chart Demo', route: '/simple-bar-chart-demo', icon: <AddToPhotosOutlinedIcon />, description: "Demo chart"  },
+    { name: 'Stacked Bar Chart Demo', route: '/stacked-bar-chart-demo', icon: <AddToPhotosOutlinedIcon />, description: "Demo chart"  }
 ];
+
