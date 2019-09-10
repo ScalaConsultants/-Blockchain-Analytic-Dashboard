f44c4bf3188fa9f135eda814727a876e35c07e25
Agata Kulbicka <agata.kulbicka@scalac.io>
2019-09-10 08:36:56 +0200

    BV-19 Lint - Containers: Home - fixed rules

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 
-const Home = (): React.ReactElement => {
-  return (
-    <div
-      style={{
-        textAlign: 'center'
-      }}
-    >
-      <h1>Blockchain Analytic Dashboard</h1>
-      <h3>
-        <a
-          href="https://github.com/ScalaConsultants/Blockchain-Analytic-Dashboard"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          https://github.com/ScalaConsultants/Blockchain-Analytic-Dashboard
-        </a>
-      </h3>
-    </div>
-  );
-};
+const Home = (): React.ReactElement => (
+  <div
+    style={{
+      textAlign: 'center'
+    }}
+  >
+    <h1>Blockchain Analytic Dashboard</h1>
+    <h3>
+      <a
+        href="https://github.com/ScalaConsultants/Blockchain-Analytic-Dashboard"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        https://github.com/ScalaConsultants/Blockchain-Analytic-Dashboard
+      </a>
+    </h3>
+  </div>
+);
 
 export default Home;
