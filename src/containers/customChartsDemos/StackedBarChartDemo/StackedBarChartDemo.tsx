3b29a2a4d6aaa65549efe626d877266c271d316d
Agata Kulbicka <agata.kulbicka@scalac.io>
2019-09-11 07:46:06 +0200

    BV-19 Containers: customChartsDemos: SimpleBarChartDemo - fixed rules

--- src/containers/customChartsDemos/StackedBarChartDemo/StackedBarChartDemo.tsx
+++ src/containers/customChartsDemos/StackedBarChartDemo/StackedBarChartDemo.tsx
@@ -20,7 +20,7 @@ const totalWalletsAmounts = (groupingKey: string, blockchain: any) =>
     }
     const newItem = {
       key: next[groupingKey],
-      value: next.amount,
+      value: next.amount
     };
 
     return [...acc, newItem];
@@ -30,33 +30,33 @@ const TEZOS_EXCHANGE_RATE = 1.05;
 const ETHEREUM_EXCHANGE_RATE = 183.48;
 
 function SimpleBarChartDemo(): React.ReactElement {
-  const { tezos, ethereum } = useMappedState(mapState);
+  const {tezos, ethereum} = useMappedState(mapState);
 
   return (
     <>
-      <h1 style={{ marginBottom: 60 }}>Stacked bar chart demo</h1>
+      <h1 style={{marginBottom: 60}}>Stacked bar chart demo</h1>
 
       <h2>Top buyers and sellers</h2>
       <h3>
         Wallets by total transactions values and its relation to the entire downloaded range
       </h3>
       <h3>Tezos</h3>
-      <h4 style={{ marginBottom: 12 }}>Buying transactions</h4>
+      <h4 style={{marginBottom: 12}}>Buying transactions</h4>
       <StackedBarChart
         data={totalWalletsAmounts('source', tezos).sort((a: any, b: any) => b.value - a.value)}
       />
-      <h4 style={{ marginBottom: 12 }}>Selling transactions</h4>
+      <h4 style={{marginBottom: 12}}>Selling transactions</h4>
       <StackedBarChart
         data={totalWalletsAmounts('destination', tezos).sort((a: any, b: any) => b.value - a.value)}
       />
       <p>(grey area are values that are less then 0.1% of total)</p>
 
       <h3>Ethereum</h3>
-      <h4 style={{ marginBottom: 12 }}>Buying transactions</h4>
+      <h4 style={{marginBottom: 12}}>Buying transactions</h4>
       <StackedBarChart
         data={totalWalletsAmounts('source', ethereum).sort((a: any, b: any) => b.value - a.value)}
       />
-      <h4 style={{ marginBottom: 12 }}>Selling transactions</h4>
+      <h4 style={{marginBottom: 12}}>Selling transactions</h4>
       <StackedBarChart
         data={totalWalletsAmounts('destination', ethereum).sort((a: any, b: any) => b.value - a.value)}
       />
@@ -67,12 +67,14 @@ function SimpleBarChartDemo(): React.ReactElement {
         data={[
           {
             name: 'Tezos',
-            value: totalWalletsAmounts('source', tezos).reduce((acc: any, next: any) => acc + next.value, 0) / 1000000 * TEZOS_EXCHANGE_RATE
+            value: totalWalletsAmounts('source', tezos).reduce((acc: any, next: any) =>
+              acc + next.value, 0) / 1000000 * TEZOS_EXCHANGE_RATE
           },
           {
             name: 'Ethereum',
-            value: totalWalletsAmounts('source', ethereum).reduce((acc: any, next: any) => acc + next.value, 0) * ETHEREUM_EXCHANGE_RATE
-          },
+            value: totalWalletsAmounts('source', ethereum).reduce((acc: any, next: any) =>
+              acc + next.value, 0) * ETHEREUM_EXCHANGE_RATE
+          }
         ]}
       />
       <p>Value in USD (counted for 1 XTZ = {TEZOS_EXCHANGE_RATE}$, 1 ETH = {ETHEREUM_EXCHANGE_RATE}$)</p>
