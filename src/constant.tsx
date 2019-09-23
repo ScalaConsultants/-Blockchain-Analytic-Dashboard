58b0d94ce31386ec8ddabb08ee287b3be137f8a5
Wojciech Nowakowski <wojciech.nowakowski94@gmail.com>
2019-09-23 12:40:27 +0200

    Remove menu labels

--- src/constant.tsx
+++ src/constant.tsx
@@ -11,10 +11,8 @@ import TimelineIcon from '@material-ui/icons/Timeline';
 import { MenuItemType } from "./types";
 
 export const menuItems: MenuItemType[] = [
-    { name: "Home", route: "/", icon: <Home />, description: "Home Page" },
     { name: "Transactions", route: "/transactions", icon: <ListIcon />, description: "List of the last 100 000 transaction" },
     { name: 'Line chart', route: '/line-chart' , icon: <TimelineIcon />, description: "Line Chart"},
-    { name: 'Simple Bar Chart Demo', route: '/simple-bar-chart-demo', icon: <AddToPhotosOutlinedIcon />, description: "Demo chart"  },
     { name: 'Stacked Bar Chart Demo', route: '/stacked-bar-chart-demo', icon: <AddToPhotosOutlinedIcon />, description: "Demo chart"  }
 ];
 
