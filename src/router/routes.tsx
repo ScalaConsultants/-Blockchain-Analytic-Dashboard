9cfcd1c661f60305598c766cc162af84ab818b10
Wojciech Nowakowski <wojciech.nowakowski94@gmail.com>
2019-09-23 12:34:48 +0200

    Remove home component

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route } from 'react-router';
 import { Container } from '@material-ui/core';
 import Transactions from '../containers/Transactions/Transactions';
-import Home from '../containers/Home/Home';
 import LineCharts from '../containers/Charts/Line';
 import SimpleBarChartDemo from '../containers/customChartsDemos/SimpleBarChartDemo/SimpleBarChartDemo';
 import StackedBarChartDemo from '../containers/customChartsDemos/StackedBarChartDemo/StackedBarChartDemo';
@@ -10,10 +9,9 @@ import StackedBarChartDemo from '../containers/customChartsDemos/StackedBarChart
 export default (): React.ReactElement => {
   const routes = (
     <Container>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={LineCharts} />
       <Route exact path="/transactions" component={Transactions} />
       <Route exact path="/line-chart" component={LineCharts} />
-      <Route exact path="/simple-bar-chart-demo" component={SimpleBarChartDemo} />
       <Route exact path="/stacked-bar-chart-demo" component={StackedBarChartDemo} />
     </Container>
   );
