ce57b4d13837b8509f00155185f69ce6fd5ca10e
Wojciech Nowakowski <wojciech.nowakowski94@gmail.com>
2019-09-25 16:34:25 +0200

    Some refactor

--- src/components/LineChart/Line-view.test.tsx
+++ src/components/LineChart/Line-view.test.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import BarChart from './Bar';
+import LineChart from './Line-view';
 
-describe('Bar', () => {
+describe('Line', () => {
   const props = {
     data: 'test'
   };
 
   it('should base render', () => {
-    const wrapper = shallow(<BarChart {...props}/>);
+    const wrapper = shallow(<LineChart {...props}/>);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render once', () => {
-    const wrapper = shallow(<BarChart {...props}/>);
+    const wrapper = shallow(<LineChart {...props}/>);
     expect(wrapper).toHaveLength(1);
   });
 
   it('should render component with proper data', () => {
-    const wrapper = shallow(<BarChart {...props}/>);
+    const wrapper = shallow(<LineChart {...props}/>);
     expect(wrapper.prop('data')).toEqual('test');
   });
 });
