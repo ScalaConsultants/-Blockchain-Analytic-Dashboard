b0fb94d30404f6d350fb7bf64c6f0ac05011b471
Wojciech Nowakowski <wojciech.nowakowski94@gmail.com>
2019-10-02 11:17:12 +0200

    Add ethereum transactions

---
+++ src/store/actions/ethereum/transactions.ts
@@ -0,0 +1,14 @@
+import { ActionType } from '../../../types';
+
+export const ETHEREUM_FETCH_TRANSACTIONS = 'ETHEREUM_FETCH_TRANSACTIONS';
+
+export const ETHEREUM_SET_TRANSACTIONS = 'ETHEREUM_SET_TRANSACTIONS';
+
+export const ethereumFetchTransactions = (): ActionType => ({
+  type: ETHEREUM_FETCH_TRANSACTIONS
+});
+
+export const ethereumSetTransactions = (blocks: any): any => ({
+  type: ETHEREUM_SET_TRANSACTIONS,
+  blocks
+});
