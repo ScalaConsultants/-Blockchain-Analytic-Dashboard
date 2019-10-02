fa4ecbb4f2c3e7d27e96e48c96082c5a51a80cfc
Wojciech Nowakowski <wojciech.nowakowski94@gmail.com>
2019-10-02 12:39:30 +0200

    Add more typing

--- src/store/actions/ethereum/wallets.ts
+++ src/store/actions/ethereum/wallets.ts
@@ -1,10 +1,11 @@
 import { ActionType } from '../../../types';
+import { Wallets, setWalletsAction } from './types';
 
 export const ETHEREUM_FETCH_WALLETS = 'ETHEREUM_FETCH_WALLETS';
 
 export const ETHEREUM_SET_WALLETS = 'ETHEREUM_SET_WALLETS';
 
-export const ethereumSetWallets = (wallets:any): any => ({
+export const ethereumSetWallets = (wallets: Wallets): setWalletsAction => ({
   type: ETHEREUM_SET_WALLETS,
   wallets
 });
--- src/store/reducers/ethereum/wallets.ts
+++ src/store/reducers/ethereum/wallets.ts
@@ -1,9 +1,12 @@
 import { ETHEREUM_SET_WALLETS } from '../../actions/ethereum/wallets';
 
-const initState: any = [];
+import { Wallet } from './types';
+import { setWalletsAction } from './types';
 
-export default (state = initState, action: any): any => {
 
+const initState: Wallet[] = [];
+
+export default (state = initState, action: setWalletsAction): Wallet[] => {
   switch (action.type) {
     case ETHEREUM_SET_WALLETS:
       return [...action.wallets];
