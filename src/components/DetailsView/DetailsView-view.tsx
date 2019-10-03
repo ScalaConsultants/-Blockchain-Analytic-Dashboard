2bb5db14a4451f35bf4e5b61ad0c8fdcf95749ab
Darius <dariusz.sobkowicz@gmail.com>
2019-10-03 13:03:46 +0200

    feat: detaild view display modal

--- src/components/DetailsView/DetailsView-view.tsx
+++ src/components/DetailsView/DetailsView-view.tsx
@@ -4,12 +4,18 @@ import BarChart from '../BarChart';
 import TransactionsList from '../TransactionsList';
 import LineChart from '../LineChart';
 import Legend from '../Legend';
+import EditWalletModal from '../EditWalletModal';
 
 const DetailsView = () => {
 
     return (
-        <>
+        <>  
             <BarChart />
+            <EditWalletModal 
+                id={350054}
+                address="0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F"
+                description="This wallet belongs to market"
+                update={() => {}}/>
             <LineChart />
             <Legend />
             <TransactionsList />
