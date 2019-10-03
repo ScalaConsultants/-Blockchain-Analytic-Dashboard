3d535b1b611c0d74e786b7345c97def88c0a9e85
dariuszsobkowicz <dariusz.sobkowicz@gmail.com>
2019-10-03 16:28:43 +0200

    [feature/bv-58] edit wallet (#38)
    * fix: theme type
    
    * fix: update extended theme type
    
    * feat: added edit modal
    
    * feat: added outlined input override
    
    * feat: detaild view display modal
    
    * fix: removed unused style

---
+++ src/components/EditWalletModal/EditWalletModal-styles.tsx
@@ -0,0 +1,85 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { ExtendedTheme } from './../../types';
+
+export const EditWalletModalStyles = makeStyles((theme: ExtendedTheme) => ({
+  modal: {
+    background: 'transparent',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  paper: {
+    width: '467px',
+    background: theme.palette.background.default,
+    padding: '0px 20px 21px',
+    borderRadius: '10px',
+    '&:focus': {
+      outline: '0px solid transparent'
+    }
+  },
+  background: {
+    background: '#253152'
+  },
+  chip: {
+    padding: '4px 6px',
+    borderRadius: '4px',
+    marginRight: '15px',
+    marginTop: '15px',
+  },
+  description: {
+    border: '1px solid #569AF5',
+    borderRadius: '5px',
+    boxShadow: 'inset 0px 4px 10px rgba(15, 25, 53, 0.5)',
+    marginTop: '4px',
+    '& div': {
+      padding: '6px 9px'
+    }
+  },
+  textarea: {
+    minHeight: '44px'
+  },
+  button: {
+    width: '100px',
+    height: '26px',
+    border: '1px solid #5699F6',
+    borderRadius: '4px',
+    textTransform: 'none',
+  },
+  buttonFirst: {
+    background: 'linear-gradient(90deg, #78CC33 4.76%, #9EE32D 100%)',
+    border: 'none'
+  },
+  close: {
+    marginLeft: 'auto'
+  },
+  fonts: {
+    fontFamily: theme.constants.DEFAULT_FONT_FAMILY,
+    fontStyle: theme.constants.DEFAULT_FONT_STYLE,
+    fontWeight: theme.constants.DEFAULT_FONT_WEIGHT,
+    fontSize: '0.75rem',
+    lineHeight: '0.75rem'
+  },
+  marginTop30: {
+    marginTop: '30px'
+  },
+  marginTop15: {
+    marginTop: '15px'
+  },
+  white: {
+    color: theme.palette.common.white
+  },
+  grey: {
+    color: theme.palette.grey[400]
+  },
+  types: {
+    '& div:nth-child(2)': {
+      background: theme.constants.MARKET_COLOR
+    }
+  },
+  editButton: {
+    height: '28px',
+    textTransform: 'none',
+    borderRadius: '4px',
+    border: 'none'
+  }
+}));
