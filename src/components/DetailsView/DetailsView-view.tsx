e771fe5694b35c0864f408a6dc897acbc4cb5cfa
Darius <dariusz.sobkowicz@gmail.com>
2019-10-07 12:17:30 +0200

    fix: bar chart grid

--- src/components/DetailsView/DetailsView-view.tsx
+++ src/components/DetailsView/DetailsView-view.tsx
@@ -6,8 +6,11 @@ import Legend from '../Legend';
 import DetailsMenu from '../DetailsMenu';
 import { withRouter } from 'react-router-dom';
 import { DetailsViewProps } from './types';
+import Grid from '@material-ui/core/Grid';
+import { useDetailsMenuStyles } from './DetailsView-styles'
 
 const DetailsView = (props: DetailsViewProps) => {
+    const classes = useDetailsMenuStyles();
     const address = props.match.params.walletHash || '';
     const id: string = address && `${Math.floor(Math.random() * (30 - 1) + 1)}`;
     const [description, updateDescription] = React.useState("This wallet belongs to market");
@@ -25,7 +28,14 @@ const DetailsView = (props: DetailsViewProps) => {
                 type="market" 
                 blockchain="Ethereum"
                 updateDescription={update}/>
-            <BarChart />
+            <Grid container justify="flex-start" alignItems="center">
+                <Grid item xs={1} className={classes.label}>
+                    ETH
+                </Grid>
+                <Grid item xs={10}>
+                    <BarChart />
+                </Grid>
+            </Grid>
             <LineChart />
             <Legend />
             <TransactionsList description={description} />
