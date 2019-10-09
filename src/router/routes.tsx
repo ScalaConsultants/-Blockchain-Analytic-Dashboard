29bfdb68d7479ac76aecc5528a81a262f7a3cb07
Darius <dariusz.sobkowicz@gmail.com>
2019-10-09 22:59:20 +0200

    fix: remove filters route

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -3,12 +3,10 @@ import { Route } from 'react-router';
 import { Container } from '@material-ui/core';
 import DashboardView from '../components/DashboardView';
 import DetailsView from '../components/DetailsView';
-import Filters from '../components/Filters';
 
 export default (): React.ReactElement => {
   const routes = (
     <Container>
-      <Route exact path="/filters" component={Filters} />
       <Route exact path="/wallet/:walletHash" component={DetailsView} />
       <Route exact path="/" component={DashboardView} />
     </Container>
