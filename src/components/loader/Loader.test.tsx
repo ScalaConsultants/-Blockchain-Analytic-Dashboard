f4545d8fa68e26fe4ba523b260a95ca5521a2c39
Agata Kulbicka <agata.kulbicka@scalac.io>
2019-10-09 14:42:08 +0200

    BV-46 Loader - update unit tests

--- src/components/loader/Loader.test.tsx
+++ src/components/loader/Loader.test.tsx
@@ -1,15 +1,22 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// import Loader from './Loader';
-//
-// describe('Loader', () => {
-//   it('should render', () => {
-//     const wrapper = shallow(<Loader />);
-//     expect(wrapper).toMatchSnapshot();
-//   });
-//
-//   it('should render only once', () => {
-//     const wrapper = shallow(<Loader />);
-//     expect(wrapper).toHaveLength(1);
-//   });
-// });
+import React from 'react';
+import { shallow } from 'enzyme';
+import Loader from './Loader';
+
+describe('Loader', () => {
+  it('should render', () => {
+    const wrapper = shallow(<Loader isLoading={true}/>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render display loader', () => {
+    const wrapper = shallow(<Loader isLoading={true}/>);
+    const loader = wrapper.find('div');
+    expect(loader).toHaveLength(1);
+  });
+
+  it('should hide loader', () => {
+    const wrapper = shallow(<Loader isLoading={false}/>);
+    const loader = wrapper.find('div');
+    expect(loader).toHaveLength(0);
+  });
+});
