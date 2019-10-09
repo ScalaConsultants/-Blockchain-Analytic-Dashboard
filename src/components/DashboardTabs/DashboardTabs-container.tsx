2a49241b22521bd82be315101276c878f10d3900
Darius <dariusz.sobkowicz@gmail.com>
2019-10-10 00:48:12 +0200

    feat: added main tabs

--- src/components/DashboardTabs/DashboardTabs-container.tsx
+++ src/components/DashboardTabs/DashboardTabs-container.tsx
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import clsx from 'clsx';
+import Grid from '@material-ui/core/Grid';
+import { useDashboardStyles } from './DashboardTabs-styles';
 
 const DashboardTabs = () => {
+  const classes = useDashboardStyles();
 
-  return <div>DashboardTabs</div>
+  const rootClasses = clsx([classes.fonts, classes.grey, classes.margin]);
+
+  const tabsList = ['Buying', 'Selling', 'Data'];
+  
+  const [active, setActive] = useState(0);
+
+  const onClick = (index: number) => setActive(index);
+
+  const createTabs = () => tabsList.map((name, index) => {
+    const underlineClasses = clsx([classes.underline], {
+      [classes.active]: index === active
+    });
+    const tabClasses = clsx(classes.tab, {
+      [classes.white]: index === active
+    })
+    return (
+      <Grid 
+        container 
+        xs={4} 
+        justify="center" 
+        className={tabClasses}
+        key={name}
+        onClick={() => onClick(index)}
+      >
+        <Grid>{name}</Grid>
+        <Grid className={underlineClasses}></Grid>
+      </Grid>
+    )
+  });
+
+  return (
+    <Grid container className={rootClasses} spacing={2}>
+      <Grid container justify="center" xs={3}>
+        {createTabs()}
+      </Grid>
+      <Grid container xs={8} justify="flex-end">
+        {/* TODO: import component */}
+        Volume type:
+      </Grid>
+    </Grid>
+  )
 };
 
 export default DashboardTabs;
---
+++ src/components/DashboardTabs/DashboardTabs-styles.tsx
@@ -0,0 +1,39 @@
+import { makeStyles } from '@material-ui/styles';
+import { ExtendedTheme } from '../../types';
+
+export const useDashboardStyles = makeStyles((theme: ExtendedTheme) => ({
+  grey: {
+    color: theme.palette.grey[400]
+  },
+  white: {
+    color: theme.palette.common.white
+  },
+  fonts: {
+    fontFamily: theme.constants.DEFAULT_FONT_FAMILY,
+    fontSize: theme.constants.DEFAULT_FONT_SIZE,
+    fontStyle: theme.constants.DEFAULT_FONT_STYLE,
+    fontWeight: theme.constants.DEFAULT_FONT_WEIGHT,
+    lineHeight: theme.constants.DEFAULT_LINE_HEIGHT
+  },
+  underline: {
+    width: '80%',
+    height: '2px',
+    background: 'transparent',
+  },
+  active: {
+    background: 'linear-gradient(90deg, #78CC33 0%, #9EE32D 100%)',
+  },
+  tab: {
+    cursor: 'pointer',
+    textAlign: 'center',
+    '&:hover': {
+      color: theme.palette.common.white,
+      '& div:nth-child(2)': {
+        background: 'linear-gradient(90deg, #78CC33 0%, #9EE32D 100%)'
+      }
+    }
+  },
+  margin: {
+    margin: '15px 0'
+  }
+}));
