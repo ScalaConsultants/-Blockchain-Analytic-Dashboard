50613311036630c126826bfa3681ec2164db8392
Darius <dariusz.sobkowicz@gmail.com>
2019-10-10 01:36:34 +0200

    refactor: tabs

--- src/components/DashboardTabs/DashboardTabs-container.tsx
+++ src/components/DashboardTabs/DashboardTabs-container.tsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import clsx from 'clsx';
 import Grid from '@material-ui/core/Grid';
+import DashboardTabsView from './DashboardTabs-view';
 import { useDashboardStyles } from './DashboardTabs-styles';
 
 const DashboardTabs = () => {
   const classes = useDashboardStyles();
-
-  const rootClasses = clsx([classes.fonts, classes.grey, classes.margin]);
 
   const tabsList = ['Buying', 'Selling', 'Data'];
   
@@ -14,7 +13,7 @@ const DashboardTabs = () => {
 
   const onClick = (index: number) => setActive(index);
 
-  const createTabs = () => tabsList.map((name, index) => {
+  const tabs = tabsList.map((name, index) => {
     const underlineClasses = clsx([classes.underline], {
       [classes.active]: index === active
     });
@@ -23,30 +22,23 @@ const DashboardTabs = () => {
     })
     return (
       <Grid 
-        container 
-        xs={4} 
-        justify="center" 
-        className={tabClasses}
+        item xs={4}
         key={name}
-        onClick={() => onClick(index)}
       >
-        <Grid>{name}</Grid>
-        <Grid className={underlineClasses}></Grid>
+        <Grid
+          container
+          justify="center"
+          className={tabClasses}
+          onClick={() => onClick(index)}
+        >
+          <Grid item>{name}</Grid>
+          <Grid item className={underlineClasses}></Grid>
+        </Grid>
       </Grid>
     )
   });
 
-  return (
-    <Grid container className={rootClasses} spacing={2}>
-      <Grid container justify="center" xs={3}>
-        {createTabs()}
-      </Grid>
-      <Grid container xs={8} justify="flex-end">
-        {/* TODO: import component */}
-        Volume type:
-      </Grid>
-    </Grid>
-  )
+  return <DashboardTabsView tabs={tabs}/>
 };
 
 export default DashboardTabs;
---
+++ src/components/DashboardTabs/DashboardTabs-view.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import clsx from 'clsx';
+import Grid from '@material-ui/core/Grid';
+import { useDashboardStyles } from './DashboardTabs-styles';
+import { ViewProps } from './types';
+
+const DashboardTabsView = ({ tabs }: ViewProps) => {
+  const classes = useDashboardStyles();
+  const rootClasses = clsx([classes.fonts, classes.grey, classes.margin]);
+
+  return (
+    <Grid container className={rootClasses} spacing={2}>
+      <Grid item xs={3}>
+        <Grid container justify="center">
+          {tabs}
+        </Grid>
+      </Grid>
+      <Grid item xs={8}>
+        <Grid container justify="flex-end">
+          {/* TODO: import component */}
+          Volume type:
+      </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default DashboardTabsView;
