100eeb86b8b50fa8e673dfcbf7ba6b21736f0ad7
Darius <dariusz.sobkowicz@gmail.com>
2019-10-09 22:45:35 +0200

    feat: init dashboard component

---
+++ src/components/DashboardView/DashboardView-container.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const DashboardView = () => {
+
+  return <div>Dashboard</div>
+}
+
+export default DashboardView;
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router';
 import { Container } from '@material-ui/core';
+import DashboardView from '../components/DashboardView';
 import DetailsView from '../components/DetailsView';
 import Filters from '../components/Filters';
 
@@ -9,7 +10,7 @@ export default (): React.ReactElement => {
     <Container>
       <Route exact path="/filters" component={Filters} />
       <Route exact path="/wallet/:walletHash" component={DetailsView} />
-      <Route exact path="/" component={DetailsView} />
+      <Route exact path="/" component={DashboardView} />
     </Container>
   );
 
