4a2816679e77c564ead6f8c27045f7acc523e4ce
Darius <dariusz.sobkowicz@gmail.com>
2019-10-17 14:14:29 +0200

    fix: topbar

--- src/components/TopBar/TopBar-view.tsx
+++ src/components/TopBar/TopBar-view.tsx
@@ -3,19 +3,24 @@ import clsx from 'clsx';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography'
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
 import { useTopBarStyles } from './TopBar-styles';
 
 const TopBar = () => {
+  const { PUBLIC_URL } = process.env;
   const classes = useTopBarStyles();
   const classesLogo = clsx([classes.logo, classes.flex]);
   return (
     <AppBar className={classes.appBar}>
       <Toolbar className={classes.toolBar}>
         <div className={classesLogo}>
-          <img src="./icons/logo.png" className={classes.img} />
+          <img src={`${PUBLIC_URL}/icons/logo.png`} className={classes.img} />
         </div>
-        <Typography>Login</Typography>
+        <Typography variant="body1" color="secondary">
+            Log in
+          </Typography>
+          <ArrowDropDownIcon color="secondary"/>
       </Toolbar>
   </AppBar>
   )
