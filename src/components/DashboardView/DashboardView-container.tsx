52f6ba3db19ae284f444830625a1cd80d19ecfe9
Darius <dariusz.sobkowicz@gmail.com>
2019-10-17 14:15:24 +0200

    fix: change routes

--- src/components/DashboardView/DashboardView-container.tsx
+++ src/components/DashboardView/DashboardView-container.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Filters from '../DashboardFilters';
 import DashboardTabs from '../DashboardTabs';
 import DashboardTable from '../DashboardTable';
+import View from '../View';
 
 const DashboardView = () => (
-  <>
+  <View>
     <Filters />
     <DashboardTabs />
     <DashboardTable />
-  </>
+  </View>
 );
 
 export default DashboardView;
