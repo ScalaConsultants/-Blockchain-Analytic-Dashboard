82fa39a0a899727374533b7b996c42f662497721
Darius <dariusz.sobkowicz@gmail.com>
2019-10-18 08:11:30 +0200

    fix: types

--- src/components/DashboardTable/DashboardTable-container.tsx
+++ src/components/DashboardTable/DashboardTable-container.tsx
@@ -3,17 +3,16 @@ import { withRouter } from 'react-router-dom';
 
 import DashboardTableView from './DashboardTable-view';
 
-import { Table } from './types';
+import { BlockchainCell, Props } from './types';
 
-const DashboardTable = (props: any) => {
-  const { match: { params: { blockchains } } } = props;
+const DashboardTable = ({ match: { params: { blockchains }}}: Props) => {
 
   const blockchainNamesMap: any = {
     ETH: { name: 'ethereum', icon: 'eth'},
     XTZ: { name: 'tezos', icon: 'tezos'}
   }
 
-  const rows = blockchains.split(',').map((name: string): Table  => blockchainNamesMap[name]);
+  const rows = blockchains.split(',').map((name: string): BlockchainCell  => blockchainNamesMap[name]);
 
   return (
     <DashboardTableView rows={rows}/>
