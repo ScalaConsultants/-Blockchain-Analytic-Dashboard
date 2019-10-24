ce9576d04096388b8127e7fbe350b2e32b911482
Wojciech Nowakowski <wojciech.nowakowski94@gmail.com>
2019-10-24 15:30:05 +0200

    Add wallets list tables

---
+++ src/components/WalletsList/wallets.tsx
@@ -0,0 +1,80 @@
+
+export const walletsList = [
+    {
+        ID: '0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
+        title: 'Title of this wallet',
+        blockchain: 'Ethereum',
+        wallet_type: 'Market'
+
+    },
+    {
+        ID: '0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
+        title: 'Title of this wallet',
+        blockchain: 'Ethereum',
+        wallet_type: 'Market'
+
+    },{
+        ID: '0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
+        title: 'Title of this wallet',
+        blockchain: 'Ethereum',
+        wallet_type: 'Market'
+
+    },{
+        ID: '0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
+        title: 'Title of this wallet',
+        blockchain: 'Ethereum',
+        wallet_type: 'Market'
+
+    },{
+        ID: '0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
+        title: 'Title of this wallet',
+        blockchain: 'Ethereum',
+        wallet_type: 'Market'
+
+    },{
+        ID: '0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
+        title: 'Title of this wallet',
+        blockchain: 'Ethereum',
+        wallet_type: 'Market'
+
+    },{
+        ID: '0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
+        title: 'Title of this wallet',
+        blockchain: 'Ethereum',
+        wallet_type: 'Market'
+
+    },{
+        ID: '0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
+        title: 'Title of this wallet',
+        blockchain: 'Ethereum',
+        wallet_type: 'Market'
+
+    },{
+        ID: '0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
+        title: 'Title of this wallet',
+        blockchain: 'Ethereum',
+        wallet_type: 'Market'
+
+    },{
+        ID: '0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
+        title: 'Title of this wallet',
+        blockchain: 'Ethereum',
+        wallet_type: 'Market'
+
+    },{
+        ID: '0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
+        title: 'Title of this wallet',
+        blockchain: 'Ethereum',
+        wallet_type: 'Market'
+
+    },{
+        ID: '0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
+        title: 'Title of this wallet',
+        blockchain: 'Ethereum',
+        wallet_type: 'Market'
+
+    },
+
+
+
+]
