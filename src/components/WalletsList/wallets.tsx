08f0ec80cc561ed824c87162bc8a39a2c2da4cca
Wojciech Nowakowski <wojciech.nowakowski94@gmail.com>
2019-10-25 16:32:45 +0200

    Add market icons

--- src/components/WalletsList/wallets.tsx
+++ src/components/WalletsList/wallets.tsx
@@ -5,44 +5,32 @@ export const walletsList = [
         walletHash:'0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
         title: 'Title of this wallet',
         blockchain: 'Ethereum',
-        walletType: 'Market'
+        market: 'Market'
     },{
         ID: '12345',
         walletHash:'0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
         title: 'Title of this wallet',
         blockchain: 'Tezos',
-        walletType: 'Market'
+        market: 'Market'
     },{
         ID: '12345',
         walletHash:'0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
         title: 'Title of this wallet',
         blockchain: 'Tezos',
-        walletType: 'Market'
+        market: 'Private'
     },{
         ID: '12345',
         walletHash:'0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
         title: 'Title of this wallet',
         blockchain: 'Tezos',
-        walletType: 'Market'
+        market: 'Market'
     },{
         ID: '12345',
         walletHash:'0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
         title: 'Title of this wallet',
         blockchain: 'Tezos',
-        walletType: 'Market'
-    },{
-        ID: '12345',
-        walletHash:'0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
-        title: 'Title of this wallet',
-        blockchain: 'Ethereum',
-        walletType: 'Market'
-    },{
-        ID: '12345',
-        walletHash:'0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
-        title: 'Title of this wallet',
-        blockchain: 'Ethereum',
-        walletType: 'Market'
-    },
+        market: 'Market'
+    }
     
 
 
