4de7294162e03a352429db12e22f26ca65f89cd3
Wojciech Nowakowski <wojciech.nowakowski94@gmail.com>
2019-10-25 16:41:19 +0200

    Optimize styles

--- src/components/WalletsList/wallets.tsx
+++ src/components/WalletsList/wallets.tsx
@@ -5,13 +5,13 @@ export const walletsList = [
         walletHash:'0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
         title: 'Title of this wallet',
         blockchain: 'Ethereum',
-        market: 'Market'
+        market: 'Fraud'
     },{
         ID: '12345',
         walletHash:'0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
         title: 'Title of this wallet',
         blockchain: 'Tezos',
-        market: 'Market'
+        market: 'DAPP'
     },{
         ID: '12345',
         walletHash:'0xEEF8Ca40c5666e8c6645360C6A253Cb72507367F',
