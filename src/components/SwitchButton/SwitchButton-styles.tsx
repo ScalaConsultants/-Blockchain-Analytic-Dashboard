81a45380ca070e043e0322f3c0404e00ebe454c0
Wojciech Nowakowski <wojciech.nowakowski94@gmail.com>
2019-10-30 09:27:27 +0100

    Fix styles for switch button

--- src/components/SwitchButton/SwitchButton-styles.tsx
+++ src/components/SwitchButton/SwitchButton-styles.tsx
@@ -18,7 +18,7 @@ export const dashboardSwitchStyles = {
 
 export const walletListSwitchStyles = {
   switchBase: {
-    top: '3px',
+    top: 3,
     color: '#4C5367',
     '&$checked': {
       color: '#9EE32D'
@@ -29,17 +29,17 @@ export const walletListSwitchStyles = {
       background: SECONDARY_BG_COLOR
     },
   },switchThumb:{
-    width:"8px",
-    height:"8px",
+    width: 8,
+    height: 8,
   },thumb:{
-    width: '12px',
-    height: '12px',
+    width: 12,
+    height: 12,
   },
   checked: {},
   track: {
-    height: '13px',
+    height: 13,
     width: '65%',
-    background: "#253152",
+    background: SECONDARY_BG_COLOR,
     border: '1px solid #2A3450',
     boxShadow: 'inset 0px 4px 10px #0F1935'
   },
