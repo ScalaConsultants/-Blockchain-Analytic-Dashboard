31f2e7f3df4bc7900e2108d1f44eb9bc499d3991
Darius <dariusz.sobkowicz@gmail.com>
2019-10-31 06:36:21 +0100

    fix: added switch

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router';
+import { Switch, Route, Redirect } from 'react-router';
 import { Container } from '@material-ui/core';
 import DashboardView from '../components/DashboardView';
 import DetailsView from '../components/DetailsView';
@@ -10,13 +10,20 @@ export default (): React.ReactElement => {
   const routes = (
     <Container>
       <TopBar />
-      <Route exact path="/wallet/:walletSource/:walletHash/:groupBy/:blockchains/:limit/:from/:to" component={DetailsView} />
-      <Route exact path="/:groupBy/:blockchains/:limit/:from/:to" component={DashboardView} />
-      <Route exact path="/wallets-list" component={WalletsList} />
+      <Switch>
+        <Route
+          exact
+          path="/wallet/:walletSource/:walletHash/:groupBy/:blockchains/:limit/:from/:to"
+          component={DetailsView}
+        />
+        <Route exact path="/:groupBy/:blockchains/:limit/:from/:to" component={DashboardView} />
+        <Route exact path="/wallets-list" component={WalletsList} />
 
-      <Route exact path="/">
-        <Redirect to="/buyer/ETH,XTZ/10/1571054400000/1571140800000" />
-      </Route>
+        <Route exact path="/">
+          <Redirect to="/buyer/ETH,XTZ/10/1571054400000/1571140800000" />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     </Container>
   );
 
