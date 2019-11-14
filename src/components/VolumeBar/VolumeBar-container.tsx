03f9358cf7bf8494470ae0583ef1b8b6cab0642e
Wojciech Nowakowski <40352669+wojciech-nowakowski@users.noreply.github.com>
2019-11-14 14:50:14 +0100

    Add VolumeBar component (#99)
    * Add VolumeBar component
    
    * Fix styles
    
    * Fix styles2

---
+++ src/components/VolumeBar/VolumeBar-container.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import volumeBarStyles from './VolumeBar-style';
+
+const VolumeBar = (): React.ReactElement => {
+    const classes = volumeBarStyles();
+
+    return (
+        <>
+            <div className={classes.containerOutside}>
+                <div className={classes.containerInside}>
+                    <div className={classes.totalVolume}>total volume</div>
+                    <div className={classes.blokchain}>25,94011 ETH</div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default VolumeBar;
